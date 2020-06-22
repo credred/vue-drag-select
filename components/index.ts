8cab220c757d9ebb3aca8378121d737299aa9427
credred <cqiufam@gmail.com>
2020-06-22 22:35:25 +0800

    fix: production enviroment index.ts install function register incorrect component name

--- components/index.ts
+++ components/index.ts
@@ -4,8 +4,8 @@ import DragSelect from "@/DragSelect.vue";
 import DragSelectOption from "@/DragSelectOption.vue";
 
 const install = (VueInstance: typeof Vue) => {
-  VueInstance.component(DragSelect.name, DragSelect);
-  VueInstance.component(DragSelectOption.name, DragSelectOption);
+  VueInstance.component("DragSelect", DragSelect);
+  VueInstance.component("DragSelectOption", DragSelectOption);
 };
 
 if (typeof window !== "undefined" && window.Vue) {
