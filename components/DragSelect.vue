0ad8d61d41520e2df8c9ff94805f1c85474e40cd
credred <cqiufam@gmail.com>
2020-06-22 23:05:19 +0800

    fix: dragArea overflow dragSelect component area

--- components/DragSelect.vue
+++ components/DragSelect.vue
@@ -97,29 +97,18 @@ export default class DragSelect extends Vue {
     return {
       left: el.offsetLeft,
       top: el.offsetTop,
-      width: el.clientWidth,
-      height: el.clientHeight,
+      width: el.scrollWidth,
+      height: el.scrollHeight,
     };
   }
 
   _getCurrentPoint(e: MouseEvent) {
+    const selfRect = this._getSelfRect();
     const { clientLeft, offsetLeft, clientTop, offsetTop } = this.$el as HTMLElement;
 
     return {
-      x: Math.max(
-        0,
-        Math.min(
-          e.clientX - clientLeft - offsetLeft + this._scrollableParent.scrollLeft,
-          this._scrollableParent.scrollWidth
-        )
-      ),
-      y: Math.max(
-        0,
-        Math.min(
-          e.clientY - clientTop - offsetTop + this._scrollableParent.scrollTop,
-          this._scrollableParent.scrollHeight
-        )
-      ),
+      x: Math.max(0, Math.min(e.clientX - clientLeft - offsetLeft + this._scrollableParent.scrollLeft, selfRect.width)),
+      y: Math.max(0, Math.min(e.clientY - clientTop - offsetTop + this._scrollableParent.scrollTop, selfRect.height)),
     };
   }
 
