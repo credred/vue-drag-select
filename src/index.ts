b35049b747f02ac809d3b24ca691d637b5c014ea
credred <cqiufan@outlook.com>
2022-01-06 22:46:03 +0800

    feat: added index.ts as output file

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import DragSelect from './DragSelect.vue';
+import DragSelectOption from './DragSelectOption.vue';
+
+DragSelect.DragSelectOption = DragSelectOption;
+
+export default DragSelect;
+export { DragSelect, DragSelectOption };
