5e67780ad3ea7e5eef2486e071071295e98cb85e
credred <cqiufan@outlook.com>
2022-01-16 21:46:57 +0800

    perf: type stronger

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,11 @@ import DragSelectOption from './DragSelectOption.vue';
 DragSelect.DragSelectOption = DragSelectOption;
 DragSelect.install = install;
 
-export default DragSelect;
+export default DragSelect as typeof DragSelect & {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  DragSelectOption: typeof DragSelectOption;
+  install: (app: App) => void;
+};
 export { DragSelect, DragSelectOption };
 
 export function install(app: App) {
