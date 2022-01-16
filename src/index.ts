c0ad120828644b6b5588256795eb74e3b6b8a7b0
credred <cqiufan@outlook.com>
2022-01-16 21:35:28 +0800

    feat:install method attach to default method for easy to use the method of app.use

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import DragSelect from './DragSelect.vue';
 import DragSelectOption from './DragSelectOption.vue';
 
 DragSelect.DragSelectOption = DragSelectOption;
+DragSelect.install = install;
 
 export default DragSelect;
 export { DragSelect, DragSelectOption };
@@ -11,4 +12,3 @@ export function install(app: App) {
   app.component('DragSelect', DragSelect);
   app.component('DragSelectOption', DragSelectOption);
 }
-export const version = '2.0.1';
