f381f9b5cdc4fd1dd0b9bed13e3a6e23a59c19f2
credred <cqiufan@outlook.com>
2022-05-15 22:47:05 +0800

    test: more useDragArea hook test suite added

--- __tests__/hooks/useDragArea.spec.ts
+++ __tests__/hooks/useDragArea.spec.ts
@@ -1,8 +1,9 @@
 import { el, elBox, setupContainer } from '@test/_setup/setupContainer';
-import { drag } from '@test/_utils/simulate';
+import { drag, pointerdown } from '@test/_utils/simulate';
 import { addition, subtraction } from '@test/_utils/math';
 import { useDragRect } from '@/hooks/useDragRect';
 import { toRect } from '@/utils/toRect';
+import { Position } from '@/typings/internal';
 
 describe('hooks/useDragArea', () => {
   setupContainer();
@@ -18,8 +19,31 @@ describe('hooks/useDragArea', () => {
       },
     });
   });
+
+  it('rect should always be included by the paddingBox of el', async () => {
+    const { rect } = useDragRect(el);
+    // topLeft to bottomRight
+    await drag(elBox.validArea.from, addition(elBox.validArea.to, [100, 100]), {
+      onBeforeEnd() {
+        expect(rect.value).toEqual(toRect(elBox.validArea.relativeFrom, elBox.validArea.relativeTo));
+      },
+    });
+    
+    // bottomRight to topLeft
+    await drag(elBox.validArea.to, subtraction(elBox.validArea.from, [10, 10]), {
+      onBeforeEnd() {
+        expect(rect.value).toEqual(toRect(elBox.validArea.relativeTo, elBox.validArea.relativeFrom));
       },
     });
   });
 
+  it('draggable area should be paddingBox', async () => {
+    const { isDragging } = useDragRect(el);
+    // border edge
+    pointerdown(...subtraction<Position>(elBox.validArea.from, [elBox.borderLeft / 2, elBox.borderTop / 2]));
+    expect(isDragging.value).toBe(false);
+    // paddingBox edge
+    pointerdown(...addition<Position>(elBox.validArea.from, [elBox.paddingLeft / 2, elBox.paddingTop / 2]));
+    expect(isDragging.value).toBe(true);
+  });
 });
